05b74c7bf3b86382aacc91192e5171c29e81a6d2
Yash Kolambekar <yashskolambekar2015@gmail.com>
2023-12-08 20:35:28 +0530

    15th file

---
+++ 015-more-complex-reptitions.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <math.h>
+
+// int main()
+// {
+//     int i;
+//     for(i = 0; i <= 10; i++){
+//         printf("%d\n", i);
+//     }
+// }
+
+// int main()
+// {
+//     int x, y;
+//     for(y = 0; y <= 9; y++){
+//         for(x = 0; x <= 9; x++){
+//             printf("%d ", y);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// int main()
+// {
+//     int i = 0;
+//     do
+//     {
+//         printf("%d", i);
+//         i++;
+//     }
+//     while(i < 4);
+// }
+
+// int main()
+// {
+//     int i, j, k;
+//     for(i = 0; i <= 1; i++)
+//     {
+//         for(j = 0; j <= 1; j++)
+//         {
+//             for(k = 0; k <= 1; k++)
+//             {
+//                 printf("%d %d %d\n", i, j, k);
+//             }
+//         }
+//     }
+// }
+
+
+// int main()
+// {
+//     int i, n;
+//     printf("\n");
+//     printf("Your number: ");
+//     scanf("%d", &n);
+//     printf("\n");
+//     for(i = 1; i <= 10; i++){
+//         printf("%d x %d = %d\n", n, i, (n * i));
+//     }
+// }
+
+// i = 2 + (y + 0.5x)
+// y = 0 = 6
+//  x = 5.5 to 12.5
+// int main()
+// {
+//     float i, y, x;
+//     printf("| y\t| x\t| i\n");
+//     for(y = 0; y <= 6; y++)
+//     {
+//         for(x = 5.5; x <= 12.5; x += 0.5){
+//             i = 2 + (y + (0.5 * x));
+//             printf("| %.2f\t| %.2f\t| %.2f\n", y, x, i);
+//         }
+//     }
+// }
