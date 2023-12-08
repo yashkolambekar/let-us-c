58adc3974e86ed30e0623ce227b65844040daa42
Yash Kolambekar <yashskolambekar2015@gmail.com>
2023-12-08 19:16:56 +0530

    loop control

---
+++ 014-loop-control.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+
+// int main()
+// {
+//     float principal, number_of_years, count;
+//     float interest, simple_interest;
+//     count = 1;
+//     while (count <= 3)
+//     {
+//         printf("\n");
+//         printf("Enter principal: ");
+//         scanf("%f", &principal);
+//         printf("Enter the time period in years: ");
+//         scanf("%f", &number_of_years);
+//         printf("Enter the interest rate: ");
+//         scanf("%f", &interest);
+//         printf("\n");
+//         simple_interest = (principal * number_of_years * interest) / 100;
+//         printf("The simple interest is %f", simple_interest);
+//         printf("\n");
+//         count += 1;
+//     }
+// }
+
+// int main()
+// {
+//     double base, power, count;
+//     while(1){
+//         printf("\n");
+//         printf("Enter base value: ");
+//         scanf("%lf", &base);
+//         if(base == 0){
+//             break;
+//         }
+//         printf("Enter power value: ");
+//         scanf("%lf", &power);
+//         printf("The answer is: %lf", pow(base, power));
+//         printf("\n");
+//     }
+// }
+
+// int main()
+// {
+//     int count = 0;
+//     while(count <= 255)
+//     {
+//         printf("%c ", count);
+//         count++;
+//     }
+// }
+
